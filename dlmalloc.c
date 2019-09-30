305819c811fd72079af2e86c3f539cf5a760821e
Alkaid <zgf574564920@gmail.com>
2019-09-30 19:27:44 -0400

    Add conditional macro to suppress compiler macro redefinition warning

--- dlmalloc.c
+++ dlmalloc.c
@@ -729,9 +729,11 @@ MAX_RELEASE_CHECK_RATE   default: 4095 unless not HAVE_MMAP
   malloc does support the following options.
 */
 
+#ifndef HAVE_USR_INCLUDE_MALLOC_H
 #define M_TRIM_THRESHOLD     (-1)
-#define M_GRANULARITY        (-2)
 #define M_MMAP_THRESHOLD     (-3)
+#endif
+#define M_GRANULARITY        (-2)
 
 /* ------------------------ Mallinfo declarations ------------------------ */
 
